1225dfaf6cd47c2021af8081a481d23f692c8688
Joshua Kennedy <josh@outthinkgroup.com>
2019-04-02 11:08:22 -0400

    changed listener port

--- index.js
+++ index.js
@@ -10,19 +10,12 @@ app.get('/', (req, res, next)=>{
     res.send('hello go to <a href="http://localhost:2900/scrape">scrape</a>')
 
 })
-app.post('/slack-get-date',(req, res, next)=>{
-    res.send(Date.now())
-})
+
 app.get('/slack-get-date',(req, res, next)=>{
-    res.send(Date.now())
+    res.send('your at /slack-get-data')
 })
 app.get('/scrape', async (req, res, next)=>{
-    const theCount = await getCount()
-    //console.log(theCount)
-    //res.send(theCount)
-
-    /* db.update('count', n=>n+1).write()
-    res.json(db) */
+    res.send('your at /scrape')
 })
 
 
