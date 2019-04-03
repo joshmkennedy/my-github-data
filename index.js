7654687e8365079d9185634299119d84e8cf83a5
Joshua Kennedy <josh@outthinkgroup.com>
2019-04-02 20:28:51 -0400

    made it post in channel

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import { getCount, getFacts } from './scraper'
 import db from './db'
 import cors from 'cors'
 import {format} from 'date-fns'
+import bodyParser from 'body-parser'
 
 const app = express()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.use(cors())
  
 app.get('/', (req, res, next)=>{
@@ -42,6 +45,44 @@ app.get('/slack-get-date',(req, res, next)=>{
 })
 app.get('/scrape', async (req, res, next)=>{
     res.send('your at /scrape')
+})
+app.post('/poll', urlencodedParser,(reg, res, next)=>{
+    res.status(200).type('json')
+    const message ={
+        "text": "This is your first interactive message",
+        "attachments": [
+            {
+                "text": "Building buttons is easy right?",
+                "fallback": "Shame... buttons aren't supported in this land",
+                "callback_id": "button_tutorial",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "yes",
+                        "text": "yes",
+                        "type": "button",
+                        "value": "yes"
+                    },
+                    {
+                        "name": "no",
+                        "text": "no",
+                        "type": "button",
+                        "value": "no"
+                    },
+                    {
+                        "name": "maybe",
+                        "text": "maybe",
+                        "type": "button",
+                        "value": "maybe",
+                        "style": "danger"
+                    }
+                ]
+            }
+        ]
+    }
+    res.json(message)
+    
 })
 
 
