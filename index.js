72d8c7efa3908c815ff8aa94e4dc0e2683ea5d50
Joshua Kennedy <josh@outthinkgroup.com>
2019-04-04 12:10:55 -0400

    fixed the poll added slack api lib

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ import cors from 'cors'
 import {format} from 'date-fns'
 import bodyParser from 'body-parser'
 import axios from 'axios';
+/* import Slack from 'slack' */
+import { WebClient } from '@slack/web-api'
+import { createMessageAdapter } from '@slack/interactive-messages'
 
 const app = express()
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const TOKEN  = 'xoxp-2534618128-43909772064-588529625937-75d246400b776ccd7c7974d31030fdf9';
+const SECRET = '76bd1090ee54432d0605ce6553fd7239';
+/* const slack = new Slack({TOKEN}) */
+const web = new WebClient(TOKEN)
+const slackInteractions = createMessageAdapter(SECRET);
 
 app.use(cors())
+/* app.use('/poll-action', slackInteractions.expressMiddleware()); */
  
 app.get('/', (req, res, next)=>{
     res.send('hello go to <a href="http://localhost:2900/scrape">scrape</a>')
@@ -47,58 +56,68 @@ app.get('/slack-get-date',(req, res, next)=>{
 app.get('/scrape', async (req, res, next)=>{
     res.send('your at /scrape')
 })
-app.post('/poll', urlencodedParser,(req, res, next)=>{
+app.post('/poll', urlencodedParser, async (req, res, next)=>{
     
-    res.status(200).type('json')
+    res.status(200).type('json').send('')
+    const attachments = [
+        {
+            "text": req.body.text,
+            "fallback": "Shame... buttons aren't supported in this land",
+            "callback_id": "the_poll",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "yes",
+                    "text": "yes",
+                    "type": "button",
+                    "value": "yes"
+                },
+                {
+                    "name": "no",
+                    "text": "no",
+                    "type": "button",
+                    "value": "no"
+                },
+                {
+                    "name": "maybe",
+                    "text": "maybe",
+                    "type": "button",
+                    "value": "maybe",
+                    "style": "danger"
+                }
+            ]
+        }
+    ]
+    const channel= req.body.channel_id
+    const text= 'Just a Quick Poll'
+    const send = await web.chat.postMessage({ channel, text, attachments })
+    /* console.log('Message sent', send.ts) */
 
-    //const {text}= JSON.parse(req.body.payload)
-    
-    const message ={
-        response_type:"in_channel",
-        "text": "Just a quick question",
-        "attachments": [
-            {
-                "text": req.body.text,
-                "fallback": "Shame... buttons aren't supported in this land",
-                "callback_id": "button_tutorial",
-                "color": "#3AA3E3",
-                "attachment_type": "default",
-                "actions": [
-                    {
-                        "name": "yes",
-                        "text": "yes",
-                        "type": "button",
-                        "value": "yes"
-                    },
-                    {
-                        "name": "no",
-                        "text": "no",
-                        "type": "button",
-                        "value": "no"
-                    },
-                    {
-                        "name": "maybe",
-                        "text": "maybe",
-                        "type": "button",
-                        "value": "maybe",
-                        "style": "danger"
-                    }
-                ]
-            }
-        ]
-    }
-    res.json(message)
-    
 })
-app.post('/poll-action',urlencodedParser, async(req, res, next)=>{
-    res.status(200).type('json')
-    console.log(req.body.payload)
-    const {user, actions, response_url} = JSON.parse(req.body.payload)
-    await axios.post(response_url, {
-        text:`@${user.name} said ${actions[0].name}`,
-        "replace_original": false
-    })
+/* slackInteractions.action('the_poll', (payload, respond) => {
+    const {text, actions, user} = payload.original_message.attachments[0]
     
+
+    console.log({text, attachments}, payload)
+    respond({attachments})
+    
+}) */
+app.post('/poll-action', urlencodedParser, async(req, res, next)=>{
+    res.status(200).type('json').send('')
+    const {channel, message_ts, original_message, user, actions} = JSON.parse(req.body.payload)  
+    console.log( JSON.parse(req.body.payload) )    
+    const attachments = original_message.attachments
+    const updatedAttachments = [...attachments, {text:`${user.name} said ${actions[0].name}`}]
+    const send = await web.chat.update({
+        channel:channel.id, 
+        text:original_message.text, 
+        attachments:updatedAttachments, 
+        ts:message_ts
+    })
+    console.log(original_message.attachments)
+
+
 })
 
 
