5571b728d31ed93700d4abf71ec331e1c1c02903
Joshua Kennedy <josh@outthinkgroup.com>
2019-04-12 16:17:29 -0400

    again

--- githubData.js
+++ githubData.js
@@ -30,20 +30,23 @@ async  function getRepos(user){
 }
 
 async function getCommitsByWeek(full_name, err) {
-    const url = await `https://api.github.com/repos/${full_name}/stats/contributors`
-    const {data} = await axios.get(url , AUTH)
-    if(err)return {w:0, additions:0, deletions:0, sum:0}
-    const { weeks } =await data[0]
-    const formated = await weeks.map(week=>{
-        const d = new Date(week.w*1000)
-        const w = format(d, 'MM/DD/YYYY')
-        const additions = week.a 
-        const deletions = week.d
-        const sum = additions - deletions 
-        return {w ,additions, deletions, sum}
-    })
-    const notInitial =  await formated.filter((repo, i)=>i!==0)
-    return notInitial
+    try{
+        const url = await `https://api.github.com/repos/${full_name}/stats/contributors`
+        const {data} = await axios.get(url , AUTH)
+        const { weeks } =await data[0]
+        const formated = await weeks.map(week=>{
+            const d = new Date(week.w*1000)
+            const w = format(d, 'MM/DD/YYYY')
+            const additions = week.a 
+            const deletions = week.d
+            const sum = additions - deletions 
+            return {w ,additions, deletions, sum}
+        })
+        const notInitial =  await formated.filter((repo, i)=>i!==0)
+        return notInitial
+    } catch(error){
+        return {w:0 ,additions:0, deletions:0, sum:0}
+    }
 }
 
 
